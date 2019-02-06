cafb2be81b17a195282c95015273e057904c2469
Raine Wallace <rainebeaudubs@gmail.com>
2019-02-06 18:35:02 -0500

    Getting index.js set up and getting a 'redux skeleton' put together.

---
+++ friends/src/reducers/reducers.js
@@ -0,0 +1,14 @@
+
+
+const initialState = {
+  friends: []
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
